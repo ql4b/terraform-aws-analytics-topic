9ba5fd39b7e6b4d631fd0d67b561b25f8e6b409d
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-27 20:02:13 +0200

    feat: conditionally create resources using the enabled variable

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 locals {
   id                       = module.this.id
   context                  = module.this.context
-
   enabled                  = var.enabled
+  
   event_producer_role_arn  = var.event_producer_role_arn
 }
 
@@ -10,7 +10,7 @@ module sns_topic {
     source      = "cloudposse/sns-topic/aws"
     version     = "1.2.0"
 
-    enabled     = local.enabled
+    count       = local.enabled ? 1 : 0 
     context     = module.this.context
 
     allowed_iam_arns_for_sns_publish = [
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "enabled" {
-    type = bool
-    description = "Set to false to prevent the module from creating any resources"
-    default = true
-}
-
 variable "event_producer_role_arn" {
     type = string
     description = "The ARN of the role you want to grant sns:Publish to "
