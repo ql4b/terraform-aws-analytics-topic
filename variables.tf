8e7662be2f7899707dfe8d6ac444cc511edc7289
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-27 19:59:21 +0200

    fix: remove unused variable

--- variables.tf
+++ variables.tf
@@ -4,11 +4,6 @@ variable "enabled" {
     default = true
 }
 
-variable "pipeline_sqs_queue_arn" {
-    type = string
-    description = "the ARN of the SQS queue you want to subscrive to the SNS topic"
-}
-
 variable "event_producer_role_arn" {
     type = string
     description = "The ARN of the role you want to grant sns:Publish to "
